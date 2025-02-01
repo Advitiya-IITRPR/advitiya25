0d72f45f5605f84d23c4ebf35e0eb91174b251be
Vedant Singh Bedi <vedantbedi@gmail.com>
2025-02-01 14:05:54 +0530

    changed events

--- src/app/(events)/events/competitions/page.js
+++ src/app/(events)/events/competitions/page.js
@@ -61,7 +61,7 @@ export default function Page() {
       >
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/flight-fury.png"}
             title={"Flight Fury"}
             desc={
               "A drone racing event where pilots guide their drones through a challenging arena."
@@ -77,7 +77,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/full-throttle.png"}
             title={"Full Throttle"}
             desc={"RC ATV Racing Event"}
             tmin={3}
@@ -107,7 +107,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/infernix/Cyber forge.png"}
             title={"Cyber Forge"}
             desc={
               "An online event in which a PS related to CAD and CAE is released on online platforms like unstop."
@@ -123,7 +123,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/voltaris/code hunt.png"}
             title={"Code Hunt"}
             desc={
               "Get ready for an awesome adventure with our coding club! The challenge? Our treasure's been swiped by the tricky Deadbug, and we need your help to get it back in the CODEHUNT."
@@ -139,7 +139,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/voltaris/Algo un;lock.png"}
             title={"Algo Unlock"}
             desc={
               "Dive into the coding challenge at 'AlgoUnlock: Code for a Cure' in advitiya! Your mission: crack the alien-coded passcode. Be the hero in this digital quest, mastering the language of input and output. Join us for a thrilling race against the clock, where a simple code changes the course of destiny. Ready to unlock the cure? Let's code together!"
@@ -155,7 +155,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/infernix/Clash Royale .png"}
             title={"Clash Royale"}
             desc={
               "The tournament will consist of three stages: group stage, qualifiers, and finals. In the group stage, matches will be knockouts (one match win or defeat). The qualifiers will be conducted in best of three (bo3) format, and the finals will be conducted in best of five (bo5) format."
@@ -171,7 +171,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/infernix/BGMI.png"}
             title={"BGMI Tournament"}
             desc={
               "A team should consist of exactly four members. Students from different educational institutions can form a team. More than one team can participate from the same institution."
@@ -187,7 +187,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/infernix/Trader's Arena .png"}
             title={"Traders Arena"}
             desc={"Trade Stocks, the techie way"}
             tmin={2}
@@ -201,7 +201,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/terranova/cover page (1).png"}
             title={"Cover Page"}
             desc={
               "Participants must design a captivating event poster by combining two randomly chosen elements – one from the Genre Wheel and the other from the Age Group Wheel."
@@ -217,7 +217,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/voltaris/capture the flag.png"}
             title={"Capture The Flag"}
             desc={"Solve Cybersecurity Challenges to advance."}
             tmin={1}
@@ -231,7 +231,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/voltaris/game jam.png"}
             title={"Game Jam"}
             desc={"Build Games and Win"}
             tmin={1}
@@ -245,7 +245,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/terranova/BEYOND ZENITH.png"}
             title={"Beyond Zenith"}
             desc={"Rocket Launching event"}
             tmin={4}
@@ -259,7 +259,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/terranova/catelizing concepts.png"}
             title={"Catalyzing Concepts"}
             desc={
               "Unveiling the Infinite Potential at the Molecular Scale. This event is designed to inspire innovation and creative thinking among participants. The specific topic of the event will be unveiled at the time of the event, adding an element of anticipation and excitement. Embrace the challenge, adaptability, and creative thinking that this thrilling and intellectually stimulating experience is set to offer."
@@ -275,7 +275,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/terranova/blue print battle.png"}
             title={"Blueprint Battle"}
             desc={"Map Making using Civil Engineering Principles"}
             tmin={4}
@@ -289,7 +289,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/terranova/the bridge off.png"}
             title={"The Bridge Off"}
             desc={"Make bridges using Ice Cream Sticks"}
             tmin={2}
@@ -303,7 +303,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/terranova/circcuit chase.png"}
             title={"Circuit Chase"}
             desc={
               "The event will consist of three rounds. Round 1 and Round 2 will be a 'Treasure Hunt' where teams solve questions based on 12th-grade and JEE syllabus concepts. Round 3 will test practical knowledge as participants build circuits according to given specifications."
@@ -319,7 +319,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/terranova/Laser Shot 2.0.png"}
             title={"Laser Shot"}
             desc={"Make path for the Laser."}
             tmin={2}
@@ -333,7 +333,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/voltaris/Maths Arena.png"}
             title={"Maths Arena"}
             desc={
               "Compete in a thrilling 3-round challenge: Round 1: Solve puzzles amidst loud music, testing teamwork under pressure. Round 2: Navigate rooms of increasing difficulty, solving problems to earn points and flags. Round 3: Place strategic bets on answers, risking points to secure victory."
@@ -349,7 +349,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/voltaris/ai fusion.png"}
             title={"AI Fusion"}
             desc={
               "To test the participant’s knowledge about existing AI tools, their usage, and efficiency."
@@ -365,7 +365,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/terranova/Cooasterx.png"}
             title={"CoasterX"}
             desc={"Marble Roller Coaster Tracks"}
             tmin={3}
@@ -393,7 +393,7 @@ export default function Page() {
         </SwiperSlide> */}
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/aeris/ROBOWAR.png"}
             title={"RoboWar"}
             desc={
               "Ultimate test of engineering excellence, teamwork, strategy, and creativity— ROBO-WARS"
@@ -409,7 +409,7 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/voltaris/Codcom.png"}
             title={"CODCOM"}
             desc={"Contest style inspired by ICPC"}
             tmin={2}
@@ -423,7 +423,105 @@ export default function Page() {
         </SwiperSlide>
         <SwiperSlide>
           <EventSlide
-            img={"/assests/events/event2.png"}
+            img={"/assests/events/aeris/ROBOSOCCER.png"}
+            title={"RoboSoccer "}
+            desc={" A robotics competition where  robots utilize  sensors and control systems to play soccer."}
+            tmin={2}
+            tmax={4}
+            dmin={180}
+            dmax={240}
+            regURL={
+              "https://unstop.com/competitions/robosoccer-advitiya25-indian-institute-of-technology-ropar-1357338"
+            }
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventSlide
+            img={"/assests/events/infernix/Cadvergence .png"}
+            title={"Cadvergence "}
+            desc={"Creativity and CAD"}
+            tmin={1}
+            tmax={2}
+            dmin={120}
+            dmax={180}
+            regURL={
+              "https://unstop.com/competitions/cadvergence-advitiya25-iit-ropar-1357789"
+            }
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventSlide
+            img={"/assests/events/infernix/Pitch or Ditch.png"}
+            title={"Pitch or Ditch"}
+            desc={"Exciting Quiz on Metallurgical Documentaries"}
+            tmin={4}
+            tmax={5}
+            dmin={180}
+            dmax={240}
+            regURL={
+              "https://unstop.com/quiz/materialize-advitiya25-indian-institute-of-technology-iit-ropar-1355780"
+            }
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventSlide
+            img={"/assests/events/terranova/astro quest.png"}
+            title={"Astroquest"}
+            desc={"Quiz based on Outer Space"}
+            tmin={2}
+            tmax={4}
+            dmin={90}
+            dmax={150}
+            regURL={
+              "https://unstop.com/quiz/astroquest-indian-institute-of-technology-iit-ropar-1362748"
+            }
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventSlide
+            img={"/assests/events/terranova/color pallete.png"}
+            title={"Colour Pallete"}
+            desc={"Designing and so much more"}
+            tmin={1}
+            tmax={1}
+            dmin={120}
+            dmax={150}
+            regURL={
+              "https://unstop.com/events/color-palette-advitiya25-indian-institute-of-technology-iit-ropar-1355827"
+            }
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventSlide
+            img={"/assests/events/voltaris/cc ratedd.png"}
+            title={"CC Rated"}
+            desc={"Coding Contest"}
+            tmin={1}
+            tmax={1}
+            dmin={120}
+            dmax={150}
+            regURL={
+              "https://unstop.com/quiz/materialize-advitiya25-indian-institute-of-technology-iit-ropar-1355780"
+            }
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventSlide
+            img={"/assests/events/voltaris/predictathon.png"}
+            title={"Predictathon"}
+            desc={"ML Hackaton"}
+            tmin={1}
+            tmax={4}
+            dmin={120}
+            dmax={150}
+            regURL={
+              "https://unstop.com/hackathons/predictathon-ml-challenge-sponsored-by-coding-blocks-advitiya25-indian-institute-of-technology-ropar-1360255"
+            }
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <EventSlide
+            img={"/assests/events/terranova/Materialize.png"}
             title={"MATERIALIZE "}
             desc={"Exciting Quiz on Metallurgical Documentaries"}
             tmin={1}
